b9c7b07fde475e2de3102cede17486796ea349f8
iftikhar <iftikhar.sabillal@ai.astra.co.id>
2022-11-14 15:36:09 +0700

    task no 1

---
+++ bin/custom/training/trainingtest/gensrc/org/training/test/constants/GeneratedTrainingTestConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 14, 2022, 11:01:57 AM                   ---
+ * ----------------------------------------------------------------
+ */
+package org.training.test.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingTestConstants
+{
+	public static final String EXTENSIONNAME = "trainingtest";
+	
+	protected GeneratedTrainingTestConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
